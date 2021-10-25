64ccbe250ef05ad9b6a2d04e62b06a37cea20fe8
dawoodjee <adam.daveed@gmail.com>
2021-10-25 16:05:33 +0200

    added frappe mgr settings

---
+++ frappe_manager/frappe_manager/doctype/frappe_manager_settings/frappe_manager_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Bantoo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Frappe Manager Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ frappe_manager/frappe_manager/doctype/frappe_manager_settings/test_frappe_manager_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Bantoo and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestFrappeManagerSettings(unittest.TestCase):
+	pass
